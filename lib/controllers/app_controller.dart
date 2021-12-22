5c29cfe1406c3c5a418be8196de3d97e6ae424bf
Hoàng Nam Hải <hai@mana.vn>
2021-12-23 02:16:13 +0700

    add RevenueCat configure for demo in-app-subscription

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_app/utils/consts.dart';
 import 'package:flutter_app/utils/custom_printer.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
 import 'package:get/get.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 var logger = Logger(
   printer: CustomPrinter('AppController'),
@@ -11,6 +13,9 @@ var logger = Logger(
 class AppController extends GetxController {
   var iState = 0.obs; // Status count; used for Rx
   User? user;
+  var offerings;
+  var purchaserInfo;
+  bool isEntitlementActivated = false;
 
   void _initFirebaseEventHandler() {
     logger.v('_initFirebaseEventHandler');
@@ -30,11 +35,27 @@ class AppController extends GetxController {
     }
   }
 
+  void _initInAppPurchasing() async {
+    Purchases.setup(kRevCatApiKey, appUserId: null, observerMode: false);
+    await Purchases.setDebugLogsEnabled(true);
+    offerings = await Purchases.getOfferings();
+    Purchases.addPurchaserInfoUpdateListener((purchaserInfo) async {
+      purchaserInfo = await Purchases.getPurchaserInfo();
+      String appUserID = await Purchases.appUserID;
+      logger.w('addPurchaserInfoUpdateListener appUserID: $appUserID');
+      logger.w('purchaserInfo: $purchaserInfo');
+      (purchaserInfo.entitlements.all[kEntitlementID] != null && purchaserInfo.entitlements.all[kEntitlementID]!.isActive) ? isEntitlementActivated = true : isEntitlementActivated = false;
+      iState++;
+      logger.w('addPurchaserInfoUpdateListener entitlement: $isEntitlementActivated');
+    });
+  }
+
   @override
   void onInit() {
     super.onInit();
     _initFirebaseEventHandler();
     _signInAnonymously();
+    _initInAppPurchasing();
   }
 
   @override
--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -9,3 +9,7 @@ const kAppTitle = 'Flutter Boilerplate Pro';
 const kUrlHelpCenter = 'https://vnexpress.net/';
 const kUrlTermsOfService = 'https://vnexpress.net/';
 const kUrlPrivacyPolicy = 'https://vnexpress.net/';
+
+// RevCat configuration
+const kRevCatApiKey = 'appl_chlrKneYzuaxuFIbpHHCpTppnON';
+const kEntitlementID = 'Premium';
